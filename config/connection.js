2e00b2be3685473a7916a1acb133e51cb1ba7503
Bernadette Januska <bmjanuska@gmail.com>
2018-08-26 11:15:25 -0400

    Set up connection and orm

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,32 @@
+
+// DEPENDENCIES
+//=======================================
+var mysql = require("mysql");
+//=======================================
+
+
+// CONNECTION
+//=======================================
+// The connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password:"root",
+  database: food_db
+});
+
+// Making connection
+connection.connect(function (err){
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+//=======================================
+
+
 // EXPORTS
 //=======================================
 // Connection for ORM to use
